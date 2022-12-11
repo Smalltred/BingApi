a715f46a061bb206a1df78e73ec05b16ab255e19
Small tred <1842244757@qq.com>
2022-12-12 05:19:12 +0800

    fix:添加缓存功能减轻服务器压力

--- app.py
+++ app.py
@@ -5,54 +5,62 @@
 from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
 from gevent import pywsgi
 from v4 import EverydayBing, params4k, params1080p
+from flask_caching import Cache
 
 files = ["1080p", "4k"]
 path1080 = "/home/每日一图/1080"
 path4k = "/home/每日一图/4k"
 
 app = Flask(__name__)
-app.debug = True
 app.config["JSON_AS_ASCII"] = False
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 resolution1080 = EverydayBing(files[0], params1080p)
 resolution4k = EverydayBing(files[1], params4k)
 
 
+@cache.cached(timeout=43200)
 @app.route("/")
 def index():
     result = resolution1080.requestUrl()
     return render_template("index.html", result=result)
 
 
+@cache.cached(timeout=43200)
 @app.route("/docs")
 def readme():
     return render_template("readme.html")
 
 
+@cache.cached(timeout=43200)
 @app.route("/api/4k")
 def image4k_json():
     result = resolution4k.requestUrl()
     return jsonify(result)
 
 
+@cache.cached(timeout=43200)
 @app.route("/api/1080")
 def image1080_json():
     result = resolution1080.requestUrl()
     return jsonify(result)
 
 
+@cache.cached(timeout=43200)
 @app.route("/api/image")
 def image_api():
     result = resolution1080.requestUrl()
     return redirect(result["data"]["url"])
 
 
+@cache.cached(timeout=43200)
 @app.route("/api/image/1080/")
 def image_1080():
     result = resolution1080.requestUrl()
     return redirect(result["data"]["url"])
 
 
+@cache.cached(timeout=43200)
 @app.route("/api/image/4k/")
 def image_4k():
     result = resolution4k.requestUrl()
