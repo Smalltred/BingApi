4e76ba633a57d9846e56909d467005eb9a21aba3
Small tred <1842244757@qq.com>
2023-01-30 23:10:59 +0800

    fix:新增根据传入天数来获取该天数的图片数量

--- v4.py
+++ v4.py
@@ -9,73 +9,64 @@ import requests
 import os
 import random
 
+
 # 获取当前目录
 # 下载目录自动创建
 # 分别请求4k和 1080 p
 # 分别下载
 # 分别写入log
-params4k = {
-    "format": "js",
-    "idx": 0,
-    "n": 1,
-    "Hp": "hp",
-    "FORM": "BEHPTB",
-    "uhd": 1,
-    "uhdwidth": 3840,
-    "uhdheight": 2160,
-    "nc": 1612409408851,
-}
-
-params1080p = {
-    "cc": "zh",
-    "format": "js",
-    "idx": 0,
-    "n": 1,
-}
 
 
 class EverydayBing:
     api = "https://www.bing.com/HPImageArchive.aspx"
     url = "https://www.bing.com"
 
-    def __init__(self, path, params):
+    def __init__(self, path, resolution="1080", mun=1):
         self.path = path
-        self.params = params
+        self.params = {
+            "cc": "zh",
+            "format": "js",
+            "idx": 1,
+            "n": mun,  # 最多八张图片
+            "Hp": "hp",  # 固定参数
+            "uhd": 1,  # 高清
+        }
+        if resolution == "4k":
+            self.params["uhdwidth"] = 3840
+            self.params["uhdheight"] = 2160
 
     def parse_response(self):
-        response = self.requests_url(self.api, self.params)
+        data = []
+        image_result = []
+        response = requests.get(self.api, self.params)
         if response.status_code == 200:
-            result = response.json()["images"][0]
-            image_url = self.url + result["url"]
-            image_date = result["startdate"]
-            image_name = result["title"]
-            image_location = result["copyright"]
-            image_result = {
-                "data": {
-                    "url": image_url,
-                    "date": image_date,
-                    "title": image_name,
-                    "location": image_location,
-                },
-                "message": "成功",
-                "code": 200
-            }
+            result = response.json()["images"]
+            if len(result) >= 1:
+                for i in range(0, len(result)):
+                    image_url = self.url + result[i]["url"]
+                    image_date = result[i]["startdate"]
+                    image_name = result[i]["title"]
+                    image_location = result[i]["copyright"]
+                    temp = {
+                        "url": image_url,
+                        "date": image_date,
+                        "title": image_name,
+                        "location": image_location,
+                    }
+
+                    data.append(temp)
+                image_result = {"message": "成功", "code": 200, "data": data}
             return image_result
         else:
             errors = {
                 "message": "失败",
                 "code": 404
             }
-            return errors
+        return errors
 
     def image(self):
         images_url = self.parse_response().get("data", None).get("url", None)
         return images_url
-
-    @staticmethod
-    def requests_url(url, params):
-        response = requests.get(url, params)
-        return response
 
     @staticmethod
     def get_random_image(path):
@@ -90,9 +81,3 @@ class EverydayBing:
         curr_path = os.getcwd()
         download_path = os.path.join(curr_path, self.path)
         return download_path
-
-
-# resolution1080 = EverydayBing("", params1080p)
-# image_url = resolution1080.image()
-# image = resolution1080.requestUrl(image_url, "").content
-# print(image)
