ca068719db031bb240ddd469e7d290f8d774c808
Small tred <1842244757@qq.com>
2023-03-16 18:26:14 +0800

    add: 新增配置文件

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/16 1:17
+# @Author  : Small tred
+# @FileName: config.py
+# @Software: PyCharm
+# @Blog    : https://www.hecady.com
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1842244757@127.0.0.1:3306/bing?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 5
+    SQLALCHEMY_POOL_TIMEOUT = 10
+    SQLALCHEMY_ECHO = True
+    DEBUG = True
+    JSON_AS_ASCII = False
